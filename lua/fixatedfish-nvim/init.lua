60fc84cf87ac59da035929dcc95c1e3128037db1
john@fixatedfish.com <John Herring>
2024-04-11 20:04:36 -0400

    boiler plate shit

---
+++ lua/fixatedfish-nvim/init.lua
@@ -0,0 +1,9 @@
+local sequences = require("fixatedfish-nvim.sequences")
+
+
+local M = {}
+
+M.fib = sequences.fib
+
+return M
+
---
+++ lua/fixatedfish-nvim/sequences.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+function M.fib()
+  print("1 1 2 3 5 8 13 21 34")
+end
+
+return M
